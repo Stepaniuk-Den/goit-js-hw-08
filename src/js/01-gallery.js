5ad79aa2e38da46f508b358c10cc0139244a5237
Stepaniuk-Den <128968908+Stepaniuk-Den@users.noreply.github.com>
2023-06-13 13:59:43 +0300

    update-08hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+console.log(SimpleLightbox);
 
-console.log(galleryItems);
+const cardsList = document.querySelector('.gallery');
+
+const galleryList = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join('');
+cardsList.insertAdjacentHTML('afterbegin', galleryList);
+
+const modal = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  navText: ['◀', '▶'],
+  closeText: '✖',
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const inputEmail = form.querySelector('input');
+const inputMessage = form.querySelector('textarea');
+const KEY_FEEDBACK = 'feedback-form-state';
+
+const arr = {};
+
+inputEmail.addEventListener('input', evt => {
+  arr.email = evt.currentTarget.value;
+  localStorage.setItem(KEY_FEEDBACK, JSON.stringify(arr));
+});
+inputMessage.addEventListener('input', evt => {
+  arr.message = evt.currentTarget.value;
+  localStorage.setItem(KEY_FEEDBACK, JSON.stringify(arr));
+});
+
+// function reload (evt) {
+// if()
+// }
+// reload();
